ccf991b8274ba02d2347060c8cd1c2464c05d420
Spencer Attick <spencer.attick@gmail.com>
2025-05-22 11:23:09 -0400

    branch test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            SPEED INSIGHTS TEST
+            SPEED INSIGHTS TEST BRANCH
           </li>
         </ol>
 
