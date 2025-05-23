da132e2e43284dc16c2fad6d31372d7ce1a9786c
Spencer Attick <spencer.attick@gmail.com>
2025-05-23 15:50:12 -0400

    remove comments

--- src/middleware.js
+++ src/middleware.js
@@ -10,16 +10,13 @@ export function middleware(request) {
   const { pathname } = request.nextUrl;
   console.log("Incoming pathname:", pathname);
 
-  // Handle root path ("/")
   if (pathname === "/") {
     const url = request.nextUrl.clone();
     const territory = "test";
     const locale = "something";
     
-    // Set browser's visible URL to `/test/something`
     url.pathname = `/${territory}/${locale}`;
     
-    // Internally rewrite to `/test/something/home`
     console.log(`Rewriting / to /${territory}/${locale}/home`);
     return NextResponse.rewrite(new URL(`/${territory}/${locale}/home`, request.url));
   }
