8a11328db6026f5e32f61b55606492604c04974a
Spencer Attick <spencer.attick@gmail.com>
2025-05-28 09:06:55 -0400

    preview 2

--- src/middleware.js
+++ src/middleware.js
@@ -25,7 +25,7 @@ export function middleware(request) {
     
     url.pathname = `/${territory}/${locale}`;
     
-    console.log(`Rewriting / to /${territory}/${locale}/home`);
+    console.log(`Rewriting / to /${territory}/${locale}/home `);
     return NextResponse.rewrite(new URL(`/${territory}/${locale}/home`, request.url));
   }
 
