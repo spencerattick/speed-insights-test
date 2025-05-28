88370dbf02af91c2bcd40e30e610a5ee94c9f1f3
Spencer Attick <spencer.attick@gmail.com>
2025-05-28 10:30:43 -0400

    udpate matcher to something v0 suggested

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,28 @@
 import { NextResponse } from "next/server";
 
 export const config = {
-    matcher: [
-      /*
+  matcher: [
+    /*
        customer's matcher
        */
-      '/((?!api|_next|_vercel|fonts|examples|legal|icons|.well-known|[\\w-]+\\.\\w+).*)',
-    ],
-  };
-  
+    "/((?!api/|_next/|_vercel/|fonts/|examples/|legal/|icons/|.well-known/|favicon.ico|.*\\..*).*)",
+  ],
+};
+
 export function middleware(request) {
   const { pathname } = request.nextUrl;
-  console.log("Incoming pathname: ", pathname);
 
   if (pathname === "/") {
     const url = request.nextUrl.clone();
     const territory = "test";
     const locale = "something";
-    
+
     url.pathname = `/${territory}/${locale}`;
-    
-    console.log(`Rewriting / to /${territory}/${locale}/home `);
-    return NextResponse.redirect(new URL(`/${territory}/${locale}/home`, request.url));
+
+    console.log(`Redirecting / to /${territory}/${locale}/home `);
+    return NextResponse.redirect(
+      new URL(`/${territory}/${locale}/home`, request.url)
+    );
   }
 
   return NextResponse.next();
