00273216ea36e2d1e3a075be338d34561ff3bccf
Spencer Attick <spencer.attick@gmail.com>
2025-05-28 10:00:32 -0400

    add speed insights

--- src/app/[territory]/[locale]/layout.tsx
+++ src/app/[territory]/[locale]/layout.tsx
@@ -1,7 +1,12 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
